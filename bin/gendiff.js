2e39cb2699446b260421c0598e73fafc70dccf21
Artem Petrishchev <artem.omny@gmail.com>
2022-07-20 00:27:59 +0300

    Add help (description & version)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
-console.log('test');
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0');
+
+program.parse();
