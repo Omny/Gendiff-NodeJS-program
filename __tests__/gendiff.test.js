e64998550615808b8bb6d4864dcb102486d8e06b
Artem Petrishchev <artem.omny@gmail.com>
2022-08-22 15:17:07 +0300

    Use describe.each in tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,26 +9,17 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff two json files in default stylish format', () => {
-  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toBe(readFile('stylish.txt'));
-});
-
-test('gendiff two yaml files in default stylish format', () => {
-  expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'))).toBe(readFile('stylish.txt'));
-});
-
-test('gendiff two json files in plain format', () => {
-  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toBe(readFile('plain.txt'));
-});
-
-test('gendiff two yaml files in plain format', () => {
-  expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'plain')).toBe(readFile('plain.txt'));
-});
-
-test('gendiff two json files in json format', () => {
-  expect(gendiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toBe(readFile('json.txt'));
-});
-
-test('gendiff two yaml files in json format', () => {
-  expect(gendiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml'), 'json')).toBe(readFile('json.txt'));
+describe.each([
+  ['file1.json', 'file2.json', 'stylish', 'stylish.txt'],
+  ['file1.json', 'file2.json', 'plain', 'plain.txt'],
+  ['file1.json', 'file2.json', 'json', 'json.txt'],
+  ['file1.yaml', 'file2.yaml', 'stylish', 'stylish.txt'],
+  ['file1.yaml', 'file2.yaml', 'plain', 'plain.txt'],
+  ['file1.yaml', 'file2.yaml', 'json', 'json.txt'],
+])('Gendiff two files: %s & %s', (file1, file2, format, expectedFile) => {
+  test(`in ${format} format`, () => {
+    expect(gendiff(getFixturePath(file1), getFixturePath(file2), format)).toBe(
+      readFile(expectedFile),
+    );
+  });
 });
