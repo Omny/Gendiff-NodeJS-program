72b99b65ac4ca916f8a13bb838340919961dd6ab
Artem Petrishchev <artem.omny@gmail.com>
2022-08-22 14:39:06 +0300

    Refactoring

--- src/main.js
+++ src/main.js
@@ -4,16 +4,18 @@ import parseData from './parsers.js';
 import buildDiffTree from './buildDiffTree.js';
 import formatDiffTree from './formatters/index.js';
 
-const gendiff = (path1, path2, formatName = 'stylish') => {
-  const fullPath1 = path.resolve(process.cwd(), path1);
-  const fullPath2 = path.resolve(process.cwd(), path2);
-  const data1 = fs.readFileSync(fullPath1, 'utf-8');
-  const data2 = fs.readFileSync(fullPath2, 'utf-8');
-  const file1Extension = path.extname(fullPath1).slice(1);
-  const file2Extension = path.extname(fullPath1).slice(1);
-  const parsedData1 = parseData(data1, file1Extension);
-  const parsedData2 = parseData(data2, file2Extension);
-  const diffTree = buildDiffTree(parsedData1, parsedData2);
+const getFullFilePath = (filePath) => path.resolve(process.cwd(), filePath);
+
+const getDataFromFile = (filePath) => {
+  const data = fs.readFileSync(filePath, 'utf-8');
+  const dataFormat = path.extname(filePath).slice(1);
+  return parseData(data, dataFormat);
+};
+
+const gendiff = (filePath1, filePath2, formatName = 'stylish') => {
+  const data1 = getDataFromFile(getFullFilePath(filePath1));
+  const data2 = getDataFromFile(getFullFilePath(filePath2));
+  const diffTree = buildDiffTree(data1, data2);
   return formatDiffTree(diffTree, formatName);
 };
 
--- src/parsers.js
+++ src/parsers.js
@@ -1,14 +1,14 @@
 import yaml from 'js-yaml';
 
-const parseData = (data, extension) => {
-  switch (extension) {
+const parseData = (data, dataFormat) => {
+  switch (dataFormat) {
     case 'json':
       return JSON.parse(data);
     case 'yml':
     case 'yaml':
       return yaml.load(data);
     default:
-      throw new Error(`Wrong file extension: ${extension}`);
+      throw new Error(`Wrong data format: ${dataFormat}`);
   }
 };
 
