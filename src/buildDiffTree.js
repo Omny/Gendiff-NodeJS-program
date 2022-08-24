602ac3cdadd784623898779478f67947a779e7b5
Artem Petrishchev <artem.omny@gmail.com>
2022-08-24 12:19:47 +0300

    Change varibles names

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -7,26 +7,26 @@ const buildDiffTree = (data1 = {}, data2 = {}, addStatus = true) => {
     const value1 = data1[key];
     const value2 = data2[key];
     if (_.isObject(value1) && _.isObject(value2)) {
-      const currentValue = buildDiffTree(value1, value2);
+      const valueAfter = buildDiffTree(value1, value2);
       const status = 'not changed';
-      return [...result, [key, currentValue, status]];
+      return [...result, [key, valueAfter, status]];
     }
-    const currentValue = _.isObject(value2) ? buildDiffTree(value1, value2, false) : value2;
-    const oldValue = _.isObject(value1) ? buildDiffTree(value1, {}, false) : value1;
+    const valueAfter = _.isObject(value2) ? buildDiffTree(value1, value2, false) : value2;
+    const valueBefore = _.isObject(value1) ? buildDiffTree(value1, {}, false) : value1;
     if (value1 === value2) {
       const status = 'not changed';
-      return [...result, [key, currentValue, status]];
+      return [...result, [key, valueAfter, status]];
     }
     if (value1 !== undefined && value2 !== undefined) {
       const status = 'changed';
-      return [...result, [key, currentValue, status, oldValue]];
+      return [...result, [key, valueAfter, status, valueBefore]];
     }
     if (value1 !== undefined) {
       const status = addStatus ? 'removed' : 'not changed';
-      return [...result, [key, oldValue, status]];
+      return [...result, [key, valueBefore, status]];
     }
     const status = addStatus ? 'added' : 'not changed';
-    return [...result, [key, currentValue, status]];
+    return [...result, [key, valueAfter, status]];
   }, []);
 };
 
