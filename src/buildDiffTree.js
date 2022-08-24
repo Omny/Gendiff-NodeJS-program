119b568d437a0d109727981fbaf86e2c0a6be71d
Artem Petrishchev <artem.omny@gmail.com>
2022-08-24 23:42:48 +0300

    Remove unnecessarily variables

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -4,24 +4,21 @@ const buildDiffTree = (data1, data2) => {
   const allKeys = _.union(Object.keys(data1), Object.keys(data2));
   const sortedKeys = _.sortBy(allKeys);
   return sortedKeys.reduce((result, key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    if (_.isObject(value1) && _.isObject(value2)) {
-      const newValue = buildDiffTree(value1, value2);
+    const oldValue = data1[key];
+    const newValue = data2[key];
+    if (_.isObject(oldValue) && _.isObject(newValue)) {
       const status = 'nested';
-      return [...result, [key, newValue, status]];
+      return [...result, [key, buildDiffTree(oldValue, newValue), status]];
     }
-    const newValue = value2;
-    const oldValue = value1;
-    if (value1 === value2) {
+    if (oldValue === newValue) {
       const status = 'not changed';
       return [...result, [key, newValue, status]];
     }
-    if (value1 !== undefined && value2 !== undefined) {
+    if (oldValue !== undefined && newValue !== undefined) {
       const status = 'changed';
       return [...result, [key, newValue, status, oldValue]];
     }
-    if (value1 !== undefined) {
+    if (oldValue !== undefined) {
       const status = 'removed';
       return [...result, [key, oldValue, status]];
     }
