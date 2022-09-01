aa8c7e090250dadad1b154510aecc2c8f0775d09
Artem Petrishchev <artem.omny@gmail.com>
2022-09-01 21:59:55 +0300

    Remove doubled default value

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format <type>', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format')
   .arguments('<filepath1>, <filepath2>')
   .action((filepath1, filepath2, { format }) => {
     console.log(gendiff(filepath1, filepath2, format));
